9f9b404e4d777ab18c9f1715fa5e63e3dee12090
Majiir Paktu <majiir@nabaal.net>
2013-04-30 16:16:13 -0400

    Formatting

--- Client.java
+++ Client.java
@@ -3,21 +3,21 @@ import java.rmi.registry.Registry;
 
 public class Client {
 
-  public static void main(String[] args) {
-    String host = (args.length < 1) ? null : args[0];
-    try {
+    public static void main(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+        try {
 
-      // Get the registry from the specified host (defaults to localhost)
-      // on the port we designed for the registry in Server.
-      Registry registry = LocateRegistry.getRegistry(host, Server.REGISTRY_PORT);
+            // Get the registry from the specified host (defaults to localhost)
+            // on the port we designed for the registry in Server.
+            Registry registry = LocateRegistry.getRegistry(host, Server.REGISTRY_PORT);
 
-      // Get an object by looking up the name it was bound to.
-      Store stub = (Store) registry.lookup("Store");
+            // Get an object by looking up the name it was bound to.
+            Store stub = (Store) registry.lookup("Store");
 
-    } catch (Exception e) {
-      System.err.println("Client exception: " + e.toString());
-      e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
     }
-  }
 
 }
--- Server.java
+++ Server.java
@@ -2,40 +2,40 @@ import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
-	
+
 /**
  * An object implementing the Remote interface. This allows it to be
  * called through RMI.
  */
 public class Server implements Store {
 
-  /**
-   * This port will be assigned to your group for use on EC2. For local testing, you can use any (nonstandard) port you wish.
-   */
-  public final static int REGISTRY_PORT = 53824;
+    /**
+     * This port will be assigned to your group for use on EC2. For local testing, you can use any (nonstandard) port you wish.
+     */
+    public final static int REGISTRY_PORT = 53824;
 
-  public static void main(String args[]) {
+    public static void main(String args[]) {
 
-    try {
-      // create the RMI registry on the local machine
-      Registry registry = LocateRegistry.createRegistry(REGISTRY_PORT);
+        try {
+            // create the RMI registry on the local machine
+            Registry registry = LocateRegistry.createRegistry(REGISTRY_PORT);
 
-      // create an object we're going to call methods on remotely
-      Server obj = new Server();
+            // create an object we're going to call methods on remotely
+            Server obj = new Server();
 
-      // export the object in the registry so it can be retrieved by client,
-      // casting to the Remote interface
-      Store stub = (Store) UnicastRemoteObject.exportObject(obj, 0);
+            // export the object in the registry so it can be retrieved by client,
+            // casting to the Remote interface
+            Store stub = (Store) UnicastRemoteObject.exportObject(obj, 0);
 
-      // bind the remote object's stub in the registry
-      registry.bind("Store", stub);
+            // bind the remote object's stub in the registry
+            registry.bind("Store", stub);
 
-      System.err.println("Server ready");
-    } catch (Exception e) {
-      System.err.println("Server exception: " + e.toString());
-      e.printStackTrace();
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+
     }
-
-  }
 
 }
