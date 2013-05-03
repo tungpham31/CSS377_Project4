a5fe86661ddf0c40d7236511127d59e07a723676
Tung Pham <tungpham31@gmail.com>
2013-05-02 23:24:46 -0400

    Add finer locks (code is not complete)

---
+++ src/Book.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Book {
+    private String bookName; // Name of the book
+    private int copies; // The current number of copies that this book has
+    private Queue<Long> waitingRequests; // A queue of the buying request waiting for this book. First in first out
+    private Lock lock; // The lock associated with this book
+    private Condition condVar; // The condition variable associated with this book
+    
+    public Book(){
+        lock = new ReentrantLock();
+        condVar = lock.newCondition();
+    }
+    
+    public Book(String bookName, int copies){
+        this.bookName = bookName;
+        this.copies = copies;
+        this.waitingRequests = new LinkedList<Long>();
+        this.lock = new ReentrantLock();
+        this.condVar = lock.newCondition();
+    }
+    
+    // Get method for bookName
+    public String getBookName(){
+        return bookName;
+    }
+    
+    // Set method for copies
+    public void setCopies(int copies){
+        this.copies = copies;
+    }
+    
+    // Get method for copies 
+    public int getCopies(){
+        return copies;
+    }
+    
+    // Get method for waitingRequests
+    public Queue<Long> getWaitingRequests(){
+        return waitingRequests;
+    }
+    
+    // Add a new waiting request id to the queue
+    public void addWaitingRequest(long requestId){
+        waitingRequests.add(Long.valueOf(requestId));
+    }
+    
+    // Get the id of the first waiting request in queue
+    public long firstWaitingRequest(){
+        return waitingRequests.peek().longValue();
+    }
+    
+    // Get method for lock
+    public Lock getLock(){
+        return lock;
+    }
+    
+    // Get method for condition variable
+    public Condition getCondVar(){
+        return condVar;
+    }
+}
--- src/Server.java
+++ src/Server.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * An object implementing the Remote interface. This allows it to be called through RMI.
@@ -19,59 +23,58 @@ public class Server implements Store {
     public final static int REGISTRY_PORT = 53824;
 
     /**
-     * The set of all the available books along with its number of copies
+     * The Map mapping from bookName to the according book object
      */
-    private Map<String, Integer> books = new HashMap<String, Integer>();
-
-    /**
-     * The Map that map each book to its queue of waiting request. Requests in each queue will be
-     * first come first serve. This database is used to track which buying request should be served
-     * first when there is an inventory available
-     */
-    private Map<String, Queue<Long>> requestRank = new HashMap<String, Queue<Long>>();
+    private Map<String, Book> books = new HashMap<String, Book>();
 
     /**
      * A variable to keep track of the id of the lattest request
      */
     private long currentRequestId = -1;
-    
-    public String sayHello(){
+
+    /**
+     * A global lock for global database
+     */
+    private Lock globalLock = new ReentrantLock();
+
+    public String sayHello() {
         return "Hello";
     }
 
-    public synchronized int buy(String bookName, int copies) throws RemoteException {
+    public int buy(String bookName, int copies) throws RemoteException {
         if (copies < 0) {
             throw new RemoteException("Copies must be non-negative.");
         }
 
-        // If there is no book like that in the books database, add it in with 0 number of copies
-        // and create a new entry for it in requestRank
-        if (!books.containsKey(bookName)) {
-            books.put(bookName, 0);
-            requestRank.put(bookName, new LinkedList<Long>());
+        globalLock.lock(); // lock before accessing the global database - books
+        Book book = books.get(bookName); // get the book with this bookName
+        if (book == null) {
+            // If there is no book like that in the books database, create a new one with 0 copies
+            book = new Book(bookName, 0);
+            books.put(bookName, book); // update books database
         }
+        globalLock.unlock(); // release the lock after accessing the global database
 
-        // Get the most number of copies possible
-        int available = books.get(bookName);
+        book.getLock().lock(); // lock before accessing this book information
+        // Get the most number of copies possible to fulfill the request
+        int available = book.getCopies();
         int bought = Math.min(copies, available);
-        books.put(bookName, available - bought);
+        book.setCopies(available - bought);
 
-        // If have not bought enough copies
         if (copies > bought) {
-            // TODO: Wait and buy more.
+            // If have not bought enough copies, wait and buy more.
             // Create an id for this waiting request
             currentRequestId++;
             long requestId = currentRequestId;
-            // Add this waitingRequest to requestRank
-            requestRank.get(bookName).add(Long.valueOf(requestId));
+            book.addWaitingRequest(requestId); // add the id of this request to the queue of
+                                               // requests waiting for this book
 
-            // Define the waiting time left = 10s
-            long waitingTimeLeft = 10 * 1000; // in milliseconds
+            long waitingTimeLeft = 10 * 1000; // Define the waiting time left = 10s in millisecs
 
             while (true) {
                 long timeStartWaiting = System.currentTimeMillis(); // in millisec
                 try {
-                    wait(waitingTimeLeft);
+                    book.getCondVar().await(waitingTimeLeft, TimeUnit.MILLISECONDS);
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -79,51 +82,62 @@ public class Server implements Store {
                 // Update waiting time left right after thread continues from wait()
                 waitingTimeLeft = waitingTimeLeft - System.currentTimeMillis() + timeStartWaiting;
 
-                if (books.get(bookName) > 0 && requestRank.get(bookName).peek() == requestId) {
+                if (book.getCopies() > 0 && book.firstWaitingRequest() == requestId) {
                     // If there is available copy of this book and this request is the first in
                     // those requesting for the book, this buyer will take those
-                    available = books.get(bookName);
+                    available = book.getCopies();
                     int buyMore = Math.min(copies - bought, available);
                     bought += buyMore;
-                    books.put(bookName, available - buyMore);
-                    
-                    if (copies == bought){
-                        // If this buyer has bough enough, he will remove himself from request queue and then exit
-                        requestRank.get(bookName).poll(); 
-                        // If there is more copies available, notify other buyer waiting for this book
-                        notifyAll();
+                    book.setCopies(available - buyMore);
+
+                    if (copies == bought) {
+                        // If this buyer has bough enough, he will remove himself from request queue
+                        // and then exit
+                        book.removeFirstWaitingRequest();
+                        // If there is more copies available, notify other buyers waiting for this
+                        // book
+                        if (book.getCopies() > 0)
+                            book.getCondVar().signalAll();
                         break;
                     }
                 }
-                
-                if (waitingTimeLeft <= 0){
-                    // If the waiting time for this thread is over then simply remove its request from request queue, then exit
-                    requestRank.get(bookName).remove(Long.valueOf(requestId));
+
+                if (waitingTimeLeft <= 0) {
+                    // If the waiting time for this thread is over then simply remove its request
+                    // from request queue, then exit
+                    book.removeWaitingRequest(requestId);
                     break;
                 }
-                
+
                 // Otherwise, this thread will keep waiting for more inventory
             }
         }
+        book.getLock().unlock(); // unlock this book after accessing and modifying its information
 
         return bought;
     }
 
-    public synchronized int sell(String bookName, int copies) throws RemoteException {
+    public int sell(String bookName, int copies) throws RemoteException {
         if (copies < 0) {
             throw new RemoteException("Copies must be non-negative.");
         }
 
-        // If there is no book like that in the books database, add it in with 0 number of copies
-        // and create a new entry for it in requestRank
-        if (!books.containsKey(bookName)) {
-            books.put(bookName, 0);
-            requestRank.put(bookName, new LinkedList<Long>());
+        globalLock.lock(); // lock before accessing global database - books
+        Book book = books.get(bookName); // get the book object with this particular name
+        if (book == null) {
+            // If there is no book like that in the books database, create a new one with 0 number
+            // of copies
+            book = new Book(bookName, 0);
+            books.put(bookName, book); // update books database with the new book
         }
+        globalLock.unlock(); // release global lock after accessing global database
 
-        int before = books.get(bookName);
-        books.put(bookName, before + copies); //update books database with new copies of this book
-        notifyAll(); // notify all the buyer waiting for this books
+        book.getLock().lock(); // lock this book before accessing and modifying its content
+        int before = book.getCopies();
+        book.setCopies(before + copies); // update number of copies of this book
+        book.getCondVar().signalAll(); // signal all the buyers waiting for this book that there are new copies
+        book.getLock().unlock(); // release the lock after accessing and modifying the book's content
+        
         return before;
     }
 
